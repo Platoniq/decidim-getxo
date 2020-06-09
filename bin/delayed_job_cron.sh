2fec4b961ce7fa68c0ebed0395c251a9535e529d
Ivan Vergés <ivan@platoniq.net>
2020-06-09 17:20:14 +0200

    modify delayed job

--- bin/delayed_job_cron.sh
+++ bin/delayed_job_cron.sh
@@ -2,7 +2,7 @@
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-APP_PATH="/home/decidim/decidim-test"
+APP_PATH="/home/decidim/app-deploy/current"
 
 if ! [ -s $APP_PATH/tmp/pids/delayed_job.pid ]; then
   RAILS_ENV=production $APP_PATH/bin/delayed_job start
