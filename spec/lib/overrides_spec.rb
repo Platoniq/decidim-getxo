e6f3446bc1ce46d2644d9cd017227c700aadd57e
David <david.igon@riseup.net>
2020-12-04 11:21:36 +0100

    Upgrade/0.23.1 (#10)
    * change decidim branch to v0.23.1
    
    * add overrriden spec
    
    * upgrade geocoder
    
    * fix proposals test since now proposals have translations
    
    * add new map config to secrets
    
    * resolve review in PR #10. Change in decidim branch and api keys in secrets
    
    * new Gemfile.lock due to change in decidim branch
    
    * update md5sum of application_mailer file

---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# We make sure that the checksum of the file overriden is the same
+# as the expected. If this test fails, it means that the overriden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-core",
+    files: {
+      # mailers
+      "/app/mailers/decidim/application_mailer.rb" => "fcd6a9ad382ef76f0a7af216b13b96cb",
+      # layouts
+      "/app/views/layouts/decidim/mailer.html.erb" => "5bbe335c1dfd02f8448af287328a49dc"
+    }
+  }
+]
+
+describe "Overriden files", type: :view do
+  checksums.each do |item|
+    # rubocop:disable Rails/DynamicFindBy
+    spec = ::Gem::Specification.find_by_name(item[:package])
+    # rubocop:enable Rails/DynamicFindBy
+    item[:files].each do |file, signature|
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
